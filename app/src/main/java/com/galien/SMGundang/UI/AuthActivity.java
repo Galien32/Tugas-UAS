2066a59adfb7b031602df27baeed3c28977239c7
Hilal06 <rifaulhilal06@gmail.com>
2020-07-08 20:32:33 +0700

    update register ui

--- app/src/main/java/com/galien/SMGundang/UI/AuthActivity.java
+++ app/src/main/java/com/galien/SMGundang/UI/AuthActivity.java
@@ -1,16 +1,28 @@
 package com.galien.SMGundang.UI;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 import com.galien.SMGundang.R;
 
 public class AuthActivity extends AppCompatActivity {
+    private Fragment registerFragment = new RegisterFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.auth_view, registerFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
